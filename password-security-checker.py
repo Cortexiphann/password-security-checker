6665bdfaa3b1d123e35ec26f1e9330700cc9b6fc
Furkan Filikci <87387312+Cortexiphann@users.noreply.github.com>
2023-11-28 13:30:26 +0300

    Update password-security-checker.py

--- password-security-checker.py
+++ password-security-checker.py
@@ -1,4 +1,6 @@
-#FurkanFilikci
+import tkinter as tk
+from tkinter import messagebox
+
 def rate_password(password):
     score = 0
 
@@ -17,27 +19,42 @@ def rate_password(password):
 
     return score
 
-def evaluate_password_security(password):
+def evaluate_password_security():
+    password = entry.get()
     security_score = rate_password(password)
 
+    result_label.config(text="Password Evaluation: ")
+    
     if security_score <= 1:
-        return "Parola güvensiz. Daha karmaşık bir parola seçin."
+        message = "Your password is insecure. Choose a more complex one."
     elif security_score == 2:
-        return "Parola zayıf bir güvenlik seviyesine sahip. Daha karmaşık bir parola seçmeyi düşünebilirsiniz."
+        message = "Your password has a weak security level. Consider choosing a more complex one."
     elif security_score == 3:
-        return "Parola orta düzeyde bir güvenlik seviyesine sahip."
+        message = "Your password has a moderate security level."
     elif security_score == 4:
-        return "Parola iyi bir güvenlik seviyesine sahip."
+        message = "Your password has a good security level."
     else:
-        return "Parola güvenli."
+        message = "Your password is very secure."
 
-while True:
-    password = input("Parolanızı girin (Çıkmak için 'q' tuşuna basın): ")
+    result_label.config(text=result_label.cget("text") + "\n" + message)
 
-    if password.lower() == "q":
-        print("Çıkış yapılıyor...")
-        break
+# Creating the Tkinter application
+app = tk.Tk()
+app.title("Password Security Evaluation")
+app.geometry("400x200")
 
-    result = evaluate_password_security(password)
-    print(result)
+# Interface elements
+label = tk.Label(app, text="Enter Your Password:")
+label.pack(pady=10)
 
+entry = tk.Entry(app, show="*")
+entry.pack(pady=10)
+
+evaluate_button = tk.Button(app, text="Evaluate Password", command=evaluate_password_security)
+evaluate_button.pack(pady=10)
+
+result_label = tk.Label(app, text="", wraplength=350, justify="left")
+result_label.pack()
+
+# Running the application
+app.mainloop()
